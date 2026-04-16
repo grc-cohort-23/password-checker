b906ec54203340d903b0937a77492c9bf93e3a77
Pasha <pavloyousee@gmail.com>
2026-04-16 11:56:50 -0700

    intial commit

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -11,4 +11,33 @@ public class PasswordCheckerTest {
     assertEquals(0, 0);
   }
 
+  //Test list 
+
+  //isAlphanumeric() checks if pass word is numbers or digits
+
+    @Test
+    void testCheckIfTheAlpahnumericIsValid(){
+      //arrange
+      PasswordChecker tst = new PasswordChecker(8, 16);
+      //act
+      boolean actual = tst.isAlphanumeric("helloHosea");
+      //assert
+      assertEquals(true, actual);
+    }
+
+    @Test
+    void testCheckIfTheAlpahnumericIsNotValid(){
+      //arrange
+      PasswordChecker tst = new PasswordChecker(8, 16);
+      //act
+      boolean actual = tst.isAlphanumeric("hello-/Hosea");
+      //assert
+      assertEquals(false, actual);
+    }
+
+
+  //describePasswordLength()
+
+  //isBannedPassword()
+
 }
