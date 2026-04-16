11cf019d34cb87158bb6bbe947eff2d141e07dce
kelley <kelleycastillo@outlook.com>
2026-04-16 12:03:59 -0700

    test to see if spaces work

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -4,11 +4,22 @@ import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
 
-  @Test
-  public void testDummy() {
-    // This test is used only to validate that your VS Code setup is working
-    // Delete this test after you have written your other tests
-    assertEquals(0, 0);
-  }
+  // @Test
+  // public void testDummy() {
+  //   // This test is used only to validate that your VS Code setup is working
+  //   // Delete this test after you have written your other tests
+  //   assertEquals(0, 0);
+  // }
+  //constructor 1: Test to see if password passes with spaces  | should not
+@Test 
+public void testPasswordWithSpaces() {
+  //arrange
+    PasswordChecker checker = new PasswordChecker(8, 12);
+    String password = "pass word";
+  //act
+    boolean result = checker.isAlphanumeric(password);
+  //assert
+    assertEquals(false, result);
 
+}
 }
