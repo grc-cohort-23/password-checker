71f67ef4e8742cb19e76c1f41ad4398f4df0067b
Kate_Nguyen <Kate_nguyen>
2026-04-16 12:05:04 -0700

    tested

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -6,9 +6,14 @@ public class PasswordCheckerTest {
 
   @Test
   public void testDummy() {
-    // This test is used only to validate that your VS Code setup is working
-    // Delete this test after you have written your other tests
-    assertEquals(0, 0);
   }
 
+@Test
+  public void TestForBannedPassword() {
+    // arrange
+      PasswordChecker passwordUtils = new PasswordChecker(6, 12);
+    // act and assert
+      assertEquals(true, passwordUtils.isBannedPassword("password"));
+      assertEquals(false, passwordUtils.isBannedPassword("Password"));
+    }
 }
