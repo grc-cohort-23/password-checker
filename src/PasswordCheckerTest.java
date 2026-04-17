f6261b6ec25ba35d6347c9b405c35a84a12d959f
Sam <174657033+SamuelM007@users.noreply.github.com>
2026-04-17 19:59:44 +0000

    describePasswordLength_PasswordIs6Long_LengthMedium() is done

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.Test;
 public class PasswordCheckerTest {
 
   @Test
-  public void testIfStringInputIsTheSameLengthMedium() {
+  public void describePasswordLength_PasswordIs6Long_LengthMedium() {
     // Arrange 
     PasswordChecker counter = new PasswordChecker(6,12);
     // Act
     String actual = counter.describePasswordLength("123456");
     // Assert
-
+    assertEquals("medium",actual);
   }
 
 
